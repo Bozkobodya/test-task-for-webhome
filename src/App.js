7079026eea66ff759381c424320ef79324ca828a
Bohdan <bogdankniagnickij@gmail.com>
2021-09-13 22:58:03 +0300

    added pagination

--- src/App.js
+++ src/App.js
@@ -3,17 +3,22 @@ import { useState } from 'react';
 import './App.css';
 import axios from 'axios'
 import Coment from './components/Coment';
+import {Pagination} from '@material-ui/lab'
 
 function App() {
 
-  const url = 'https://jordan.ashton.fashion/api/goods/30/comments'
-
-  async function getComents(){
-    const response = await axios.get(url)
+  async function getComents(page = 1){
+    const response = await axios.get('https://jordan.ashton.fashion/api/goods/30/comments',{
+      params: {
+        page: page
+      }
+    })
     setComents(response.data.data)
+    setPages(response.data.last_page)
   }
 
   const [coments, setComents] = useState([])
+  let [pages, setPages] = useState(0)
 
   useEffect(() => {
     getComents()
@@ -21,6 +26,7 @@ function App() {
 
   return (
     <div className="App">
+      <Pagination onChange={(e, page) => getComents(page)} count={pages} variant="outlined" color="primary" />
       <div className="comentsList">
         {coments.map(res => {
           return <Coment name={res.name} text={res.text} created_at={res.created_at} key={res.id}/>
