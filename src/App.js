b215905f66b05f9a6e19bf019fdbdfa2a2e9ce73
Bohdan <bogdankniagnickij@gmail.com>
2021-09-14 16:29:08 +0300

    added load more

--- src/App.js
+++ src/App.js
@@ -4,34 +4,61 @@ import './App.css';
 import axios from 'axios'
 import Coment from './components/Coment';
 import {Pagination} from '@material-ui/lab'
+import {Button} from '@material-ui/core'
 
 function App() {
 
-  async function getComents(page = 1){
-    const response = await axios.get('https://jordan.ashton.fashion/api/goods/30/comments',{
+  function getComents(page){
+    axios.get('https://jordan.ashton.fashion/api/goods/30/comments',{
       params: {
         page: page
       }
+    }).then(response => {
+      setCurrentPage(currentPage = response.data.current_page);
+      setComents(coments = response.data.data);
+    setPages(pages = response.data.last_page);
+    console.log(currentPage);
     })
-    setComents(response.data.data)
-    setPages(response.data.last_page)
+    
   }
 
-  const [coments, setComents] = useState([])
+  async function loadMore(nextPage){
+    let getPage = ++nextPage
+    await axios.get('https://jordan.ashton.fashion/api/goods/30/comments',{
+      params: {
+        page: getPage
+      }
+    }).then(response => {
+      setCurrentPage(currentPage = response.data.current_page);
+      setPages(pages = response.data.last_page);
+      setLoadMoreComents(loadMoreComents = response.data.data);
+      setComents(coments = [ ...coments, ...loadMoreComents]);
+      console.log(currentPage);
+    })
+    
+  }
+
+  let [currentPage, setCurrentPage] = useState()
+  let [coments, setComents] = useState([])
   let [pages, setPages] = useState(0)
+  let [loadMoreComents, setLoadMoreComents] = useState([])
 
   useEffect(() => {
-    getComents()
+    getComents(1)
   }, [])
 
   return (
     <div className="App">
-      <Pagination onChange={(e, page) => getComents(page)} count={pages} variant="outlined" color="primary" />
       <div className="comentsList">
         {coments.map(res => {
           return <Coment name={res.name} text={res.text} created_at={res.created_at} key={res.id}/>
         })}
       </div>
+      <div className="load__button">
+        {(!(currentPage === pages)) ?
+          <Button onClick={() => loadMore(currentPage)} variant="contained" color="primary">Load more</Button> : <div></div>}
+      </div>
+      <div className="pagination"><Pagination onChange={(e, page) =>  getComents(page)} count={pages} variant="outlined" color="primary" /></div>
     </div>
   );
 }
