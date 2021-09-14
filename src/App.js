bb2ae7f74d64831781a303e3d1877c0a86751f84
Bohdan <bogdankniagnickij@gmail.com>
2021-09-14 21:49:09 +0300

    added send form

--- src/App.js
+++ src/App.js
@@ -17,14 +17,12 @@ function App() {
       setCurrentPage(currentPage = response.data.current_page);
       setComents(coments = response.data.data);
     setPages(pages = response.data.last_page);
-    console.log(currentPage);
     })
-    
   }
 
-  async function loadMore(nextPage){
+  function loadMore(nextPage){
     let getPage = ++nextPage
-    await axios.get('https://jordan.ashton.fashion/api/goods/30/comments',{
+    axios.get('https://jordan.ashton.fashion/api/goods/30/comments',{
       params: {
         page: getPage
       }
@@ -33,8 +31,24 @@ function App() {
       setPages(pages = response.data.last_page);
       setLoadMoreComents(loadMoreComents = response.data.data);
       setComents(coments = [ ...coments, ...loadMoreComents]);
-      console.log(currentPage);
     })
+  }
+
+  function sendComent(e){
+    e.preventDefault()
+    if(userText === ''){
+      setError(error = true)
+    }else if(comentText === ''){
+      setError(error = true)
+    } else {
+      axios.post('https://jordan.ashton.fashion/api/goods/30/comments',{
+        name: userText,
+        text: comentText
+      })
+      setUserText('')
+      setComentText('')
+      setError(false)
+    }
     
   }
 
@@ -43,22 +57,41 @@ function App() {
   let [pages, setPages] = useState(0)
   let [loadMoreComents, setLoadMoreComents] = useState([])
 
+  let [userText, setUserText] = useState('')
+  let [comentText, setComentText] = useState('')
+  let [error, setError] = useState(false)
+
   useEffect(() => {
     getComents(1)
   }, [])
 
   return (
     <div className="App">
-      <div className="comentsList">
-        {coments.map(res => {
-          return <Coment name={res.name} text={res.text} created_at={res.created_at} key={res.id}/>
-        })}
+      <h1 className="h1">Coments</h1>
+      <div>
+        <form className="form">
+          {error ? <div className="form__el red_text">*All fields should be filled</div> : <span/>}
+          <div className="form__el">Username:</div>
+          <div className="form__el"><input value={userText} onChange={e => setUserText(userText = e.target.value)} className="input" type="text"/></div>
+          <div className="form__el">Coment:</div>
+          <div className="form__el input"><textarea value={comentText} className="textarea" onChange={e => setComentText(comentText  = e.target.value)} rows="10" cols="45" name="text"></textarea></div>
+          <Button onClick={sendComent} variant="contained" color="primary">Add coment</Button>
+        </form>
       </div>
-      <div className="load__button">
-        {(!(currentPage === pages)) ?
-          <Button onClick={() => loadMore(currentPage)} variant="contained" color="primary">Load more</Button> : <div></div>}
+      <div className="coment__section">
+        <div className="comentsList">
+          {coments.map(res => {
+            return <Coment name={res.name} text={res.text} created_at={res.created_at} key={res.id}/>
+          })}
+        </div>
+        <div className="load__button">
+          {(!(currentPage === pages)) ?
+            <Button onClick={() => loadMore(currentPage)} variant="contained" color="primary">Load more</Button> : <div></div>}
+        </div>
+        <div className="pagination">
+          <Pagination onChange={(e, page) =>  getComents(page)} count={pages} variant="outlined" color="primary" />
+        </div>
       </div>
-      <div className="pagination"><Pagination onChange={(e, page) =>  getComents(page)} count={pages} variant="outlined" color="primary" /></div>
     </div>
   );
 }
